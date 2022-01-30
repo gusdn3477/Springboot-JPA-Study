9445cff2c8e9db692a6f81feca75f76ed00379eb
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-30 23:46:46 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,15 +14,23 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+        /*
+        플러시는?
+        - 영속성 컨테스트를 비우지 않음
+        - 영속성 컨텍스트의 변경내용을 데이터베이스에 동기화
+        - 트랜잭선이라는 작업 단위가 중요 -> 커밋 직전에만 동기화 하면 됨
+
+        플러시 후 커밋
+        FlushModeType.AUTO : 커밋이나 쿼리를 실행할 때 플러시(기본값)
+        FlushModeType.COMMIT : 커밋할 때만 플러시
+        */
         try {
             //영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
+            Member member1 = new Member(200L, "A");
             em.persist(member1);
-            em.persist(member2);
-            System.out.println("=====================");
 
+            em.flush();
+            System.out.println("=====================");
             tx.commit();
         } catch (Exception e){
             tx.rollback();
