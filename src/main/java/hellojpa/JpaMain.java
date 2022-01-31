6ca8902fbeb0698d0f08cb38b3804a36fc3672e2
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-01-31 21:18:57 +0900

    양방향 연관관계와 연관관계의 주인 1 - 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -41,10 +42,11 @@ public class JpaMain {
             em.clear();
 
             Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.getName() = " + findTeam.getName());
-
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
             tx.commit();
         } catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -13,6 +12,32 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    /*
+    mappedBy = JPA의 멘탈붕괴 난이도..
+    객체와 테이블의 관계를 맺는 차이
+    객체 연관관계 = 2개(회원->팀 단방향, 팀->회원 단방향)
+    테이블 연관관계 = 1개(회원<->팀의 연관관계 1개(양방향))
+    개게의 양방향 관계는 사실 양방향 관계가 아니라 서로 다른 단방향 관계 2개다.
+    객체를 양방향으로 참조하려면 단방향 연관관계를 2개 만들어야 한다.
+    but, 테이블은 외래키 하나로 두 테이블의 연관관계를 관리
+
+    연관관계의 주인(Owner)
+    양방향 매핑 규칙
+    - 객체의 두 관계중 하나를 연관관계의 주인으로 지정
+    - 연관관계의 주인만이 외래 키를 관리(등록, 수정)
+    - 주인이 아닌 쪽은 읽기만 가능
+    - 주인이 mappedBy 속성 사용 X
+    - 주인이 아니면 mappedBy 속성으로 주인 지정
+
+    누구를 주인으로?
+    - 외래 키가 있는 곳을 주인으로 하자.(일대 다에서 다가 주인이 됨)
+    - 진짜 매핑 - 연관관계의 주인(Member.team)
+    - 가짜 매핑 - 주인의 반대편(Team.members)
+    - 자동차와 바퀴, 자동차가 비즈니스적으로는 훨씬 중요하나 바퀴가 연관관계의 주인이 된다.
+   */
+    @OneToMany(mappedBy = "team") // 반대편에 걸려있는 변수명!(Team team)
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -28,5 +53,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
